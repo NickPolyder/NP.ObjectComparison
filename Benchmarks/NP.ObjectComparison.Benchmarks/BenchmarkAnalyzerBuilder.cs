3a3b267370191f1e78453c64fdf4c9573e150ae8
Nick Polyderopoulos <nickpolyder@hotmail.gr>
2022-03-24 02:30:26 +0000

    Benchmarks

---
+++ Benchmarks/NP.ObjectComparison.Benchmarks/BenchmarkAnalyzerBuilder.cs
@@ -0,0 +1,49 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using NP.ObjectComparison.Analyzers;
+using NP.ObjectComparison.Analyzers.Settings;
+using NP.ObjectComparison.Benchmarks;
+
+[MemoryDiagnoser]
+[Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn]
+public class BenchmarkAnalyzerBuilder
+{
+	private static readonly AnalyzerSettings _analyzerOptions;
+
+	static BenchmarkAnalyzerBuilder()
+	{
+		_analyzerOptions =  AnalyzerSettings.DefaultSettings.Invoke()
+			?? new AnalyzerSettings();
+	}
+
+	[Benchmark(Baseline = true)] // mark this is as baseline method.
+	public void FirstBuild()
+	{
+		AnalyzerBuilder<BenchmarkObject>.Build().ToArray();
+	}
+
+	[Benchmark]
+	public void WithAnalyzerOptions()
+	{
+		AnalyzerBuilder<BenchmarkObject>.Build(_analyzerOptions).ToArray();
+	}
+
+	[GlobalSetup(Targets = new[] { nameof(WhenAlreadyInitializedOnce), nameof(WhenAlreadyInitializedOnceAndOptions) })]
+	public void WhenAlreadyInitializedOnceSetup()
+	{
+		AnalyzerBuilder<BenchmarkObject>.Build().ToArray();
+	}
+	
+	[Benchmark]
+	public void WhenAlreadyInitializedOnce()
+	{
+		AnalyzerBuilder<BenchmarkObject>.Build().ToArray();
+	}
+
+	[Benchmark]
+	public void WhenAlreadyInitializedOnceAndOptions()
+	{
+		AnalyzerBuilder<BenchmarkObject>.Build(_analyzerOptions).ToArray();
+	}
+}
---
+++ Benchmarks/NP.ObjectComparison.Benchmarks/BenchmarkObject.cs
@@ -0,0 +1,75 @@
+﻿namespace NP.ObjectComparison.Benchmarks;
+
+public class BenchmarkObject :  IEquatable<BenchmarkObject>, ICloneable
+{
+	public string Id { get; set; }
+
+	public string Name { get; set; }
+
+	public int Order { get; set; }
+
+	/// <inheritdoc />
+	public bool Equals(BenchmarkObject? other)
+	{
+		if (ReferenceEquals(null, other))
+		{
+			return false;
+		}
+
+		if (ReferenceEquals(this, other))
+		{
+			return true;
+		}
+
+		return Id == other.Id && Name == other.Name && Order == other.Order;
+	}
+
+	/// <inheritdoc />
+	public override bool Equals(object? obj)
+	{
+		if (ReferenceEquals(null, obj))
+		{
+			return false;
+		}
+
+		if (ReferenceEquals(this, obj))
+		{
+			return true;
+		}
+
+		if (obj.GetType() != this.GetType())
+		{
+			return false;
+		}
+
+		return Equals((BenchmarkObject)obj);
+	}
+
+	/// <inheritdoc />
+	public override int GetHashCode()
+	{
+		return HashCode.Combine(Id, Name, Order);
+	}
+
+	/// <inheritdoc />
+	public object Clone()
+	{
+		return new BenchmarkObject
+		{
+			Id = (string)Id.Clone(),
+			Name = (string)Name.Clone(),
+			Order = Order
+		};
+	}
+
+	public static bool operator ==(BenchmarkObject? left, BenchmarkObject? right)
+	{
+		return Equals(left, right);
+	}
+
+	public static bool operator !=(BenchmarkObject? left, BenchmarkObject? right)
+	{
+		return !Equals(left, right);
+	}
+
+}
